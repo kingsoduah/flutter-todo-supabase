43533de2bc3274369d827dcde3a27edd41a623c8
Developer-Kings-dev <developerkingsdevops@gmail.com>
2025-05-14 10:55:17 +0100

    Made Changes on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://eqqtrrfpmvjizkyjdocm.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxcXRycmZwbXZqaXpreWpkb2NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMjkzOTIsImV4cCI6MjA2MjcwNTM5Mn0.4k7mOtp5eJtd67Nnr1s5COVAzwuHIUSBtP9BBAKWbjY',
+    url: '',
+    anonKey: '',
   );
   runApp(MyApp());
 }
